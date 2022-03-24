5540ca2d38c9a9549d05c80837a93044dd99f378
Derek Wong <wong.derek92@gmail.com>
2022-03-23 21:03:30 -0700

    Create function for selecting color, changing color and getting random color

--- main.js
+++ main.js
@@ -4,16 +4,23 @@
 // - Create each div element via DOM then append to grid
 // - Place into Function
 const grid = document.querySelector('.grid-squares');
+let currentColor = "black";
+let randomColor = "red";
 
 function setupGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    for (let i = 0; i < size * size; i++){
+    let total = size * size;
+
+    for (let i = 0; i < total; i++){
         let square = document.createElement('div');
+        square.classList.add("pixel");
         grid.style.backgroundColor = 'blue';
         grid.appendChild(square);
     }
+
+    changeColor();
 }
 
 // 2. Create function to take input from HTML for size / change sizes of grid
@@ -23,10 +30,38 @@ function changeSize(){
     setupGrid(size);
 }
 
-
 // 3. Create functions for random color / rainbow color button
+function changeColor() {
+    let pixels = document.querySelectorAll('.pixel');
+    
+    pixels.forEach(pixel => pixel.addEventListener('mousedown', () => {
+        if (currentColor === 'red') {
+            getRandomColor();
+            pixel.style.backgroundColor = randomColor;
+        }
+
+        else {
+            pixel.style.backgroundColor = currentColor;
+        }
+    })
+    );
+}
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    randomColor = '#';
+    for (var i = 0; i < 6; i++) {
+      randomColor += letters[Math.floor(Math.random() * 16)];
+    }
+    return randomColor;
+  }
 
 // 4. Create function for changing color 
+
+function selectColor(color){
+    currentColor = color;
+}
+
 
 // 5. Function to reset the game board
 
