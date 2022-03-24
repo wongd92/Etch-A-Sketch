be7350b16e92236bdc3acf946ac353d5b6fb8e53
Derek Wong <wong.derek92@gmail.com>
2022-03-23 18:35:22 -0700

    Set-up base documents (HTML/CSS/JS) for project

---
+++ main.js
@@ -0,0 +1,16 @@
+// 1. Set-up 16x16 grid
+// - Hard-code 16x16 to test grid then update code to make it dynamic
+// - Look-up CSS Grid/Grid Template JS
+// - Create each div element via DOM then append to grid
+// - Place into Function
+
+// 2. Create function to take input from HTML for size / change sizes of grid
+
+// 3. Create functions for random color / rainbow color button
+
+// 4. Create function for changing color 
+
+// 5. Function to reset the game board
+
+// 6. Event listeners for mousedown and click to start/stop drawing
+
